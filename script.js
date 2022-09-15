d4dd3a0f273ae3ed6da784f1836e8f37f523b4e5
LeeKien90 <lekien21090@gmail.com>
2022-09-15 10:30:39 +0700

    add

--- script.js
+++ script.js
@@ -13,7 +13,7 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
-document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.number').textContent = "?";
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -25,6 +25,7 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'Correct number';
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    document.querySelector(".number").textContent=secretNumber;
     if (score > highScore) {
       highScore = score;
       document.querySelector('.highscore').textContent = highScore;
